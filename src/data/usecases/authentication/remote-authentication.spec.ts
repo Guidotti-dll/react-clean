6b6c657ecb1084210aef3431111eefe8930adc47
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-09-09 16:28:57 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import faker from 'faker'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
@@ -17,7 +18,14 @@ describe('RemoteAuthentication', () => {
   test('should call httpPostClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('should call httpPostClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const AuthenticationParams = mockAuthentication()
+    await sut.auth(AuthenticationParams)
+    expect(httpPostClientSpy.body).toEqual(AuthenticationParams)
   })
 })
